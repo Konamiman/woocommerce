eb3871aed66fa766a8d0277148545d90ccd21f68
Néstor Soriano <konamiman@konamiman.com>
2025-05-29 08:41:21 +0200

    Backport ##81 to some-release-branch

--- plugins/woocommerce/woocommerce.php
+++ plugins/woocommerce/woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce
  * Plugin URI: https://woocommerce.com/
  * Description: An ecommerce toolkit that helps you sell anything. Beautifully.
- * Version: 10.34.0-some-release
+ * Version: 10.0.0-dev
  * Author: Automattic
  * Author URI: https://woocommerce.com
  * Text Domain: woocommerce
@@ -65,3 +65,5 @@ $GLOBALS['woocommerce'] = WC();
 if ( class_exists( \Automattic\Jetpack\Connection\Rest_Authentication::class ) ) {
 	\Automattic\Jetpack\Connection\Rest_Authentication::init();
 }
+
+// Foobar!
