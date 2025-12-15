69e14020967bd8cd9d0f12c5e8d0f31f6a680db8
Néstor Soriano <konamiman@konamiman.com>
2025-12-15 10:02:34 +0100

    Requires at least to 6.7, requires PHP to 7.3

--- plugins/woocommerce/woocommerce.php
+++ plugins/woocommerce/woocommerce.php
@@ -8,8 +8,8 @@
  * Author URI: https://woocommerce.com
  * Text Domain: woocommerce
  * Domain Path: /i18n/languages/
- * Requires at least: 6.8
- * Requires PHP: 7.4
+ * Requires at least: 6.7
+ * Requires PHP: 7.3
  *
  * @package WooCommerce
  */
